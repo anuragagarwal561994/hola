14aaa460a4967dd001e4f4ab01e0190b5d23695e
Manraj Singh Grover <manrajsinghgrover@gmail.com>
2017-10-08 15:05:16 +0530

    Almost final code

--- src/js/renderer.js
+++ src/js/renderer.js
@@ -1,5 +1,3 @@
-"use strict";
-
 var config = {
   apiKey: "AIzaSyDcatmPrGGXCQxRlrs_LHIpyKl3kZiBry0",
   authDomain: "halo-566cd.firebaseapp.com",
@@ -33,9 +31,6 @@ app.config( /*@ngInject*/ function($routeProvider) {
   $routeProvider.when('/', {
     controller: 'homeController',
     templateUrl: 'views/home.html'
-  }).when('/totp', {
-    controller: 'totpController',
-    templateUrl: 'views/totp.html'
   }).when('/dashboard', {
     controller: 'dashboardController',
     templateUrl: 'views/dashboard.html'
@@ -78,7 +73,6 @@ app.run( /*@ngInject*/ function($rootScope, $timeout, $route, $window, $location
 
   $rootScope.$on("$locationChangeStart", function(event, next, current) {
     var path = $location.path();
-    console.log('New routing called!');
 
     let session = sessionService.getSession();
 
@@ -104,7 +98,6 @@ app.run( /*@ngInject*/ function($rootScope, $timeout, $route, $window, $location
 
 
 app.controller("homeController", /*@ngInject*/ function($scope, $location, sessionService) {
-  console.log($location.path())
   firebase.auth().onAuthStateChanged(function(user) {
     console.log(user);
     if (user) {
@@ -117,31 +110,53 @@ app.controller("homeController", /*@ngInject*/ function($scope, $location, sessi
       const providerData = user.providerData;
       const totp = user.totp;
 
+      console.log(phoneNumber, uid);
+
       user.getIdToken().then((accessToken) => {
-        var ref = firebase.database().ref("totp");
-
-        ref.orderByChild('number').equalTo(phoneNumber).once('value')
-          .then(function(snapshot) {
-            console.log(snapshot.val());
-            var totp = snapshot.child("totp").val();
-            const session = {
-              displayName: displayName,
-              email: email,
-              emailVerified: emailVerified,
-              phoneNumber: phoneNumber,
-              photoURL: photoURL,
-              uid: uid,
-              accessToken: accessToken,
-              providerData: providerData,
-              authenticated: true,
-              activePage: 'dashboard',
-              totp: totp
-            };
-
-            sessionService.setSession(session);
-            $location.path('/dashboard');
-            $scope.$apply();
+        console.log(user, accessToken);
+        const databaseRef = firebase.database().ref().child('totp');
+        const querybaseRef = querybase.ref(databaseRef, ['number', 'user_id']);
+        const queriedDbRef = querybaseRef
+          .where({
+            number: phoneNumber,
+            user_id: uid
           });
+
+        queriedDbRef.on('value', (snapshot) => {
+          console.log(snapshot.val());
+
+          var totp = null;
+          var name = null;
+          var email = null;
+
+          if (snapshot.val() !== null && snapshot.val() !== undefined) {
+            const vals = Object.values(snapshot.val());
+
+            totp = vals[0]["passcode"]
+            name = vals[0]["name"]
+            email = vals[0]["email"]
+          }
+
+          console.log(totp, name, email);
+
+          const session = {
+            displayName: name,
+            email: email,
+            emailVerified: emailVerified,
+            phoneNumber: phoneNumber,
+            photoURL: photoURL,
+            uid: uid,
+            accessToken: accessToken,
+            providerData: providerData,
+            authenticated: true,
+            activePage: 'dashboard',
+            totp: totp
+          };
+
+          sessionService.setSession(session);
+          $location.path('/dashboard');
+          $scope.$apply();
+        });
       });
     } else {
       console.log('Not logged in');
@@ -213,13 +228,161 @@ app.directive('strongSecret', function() {
 });
 
 
-app.controller("totpController", /*@ngInject*/ function($scope, $location, sessionService) {
-  $scope.session = sessionService.getSession();
-});
-
-
 app.controller("speeddialController", /*@ngInject*/ function($scope, $location, sessionService) {
+  console.log('speedDial called!');
   $scope.session = sessionService.getSession();
+
+  $scope.dial = {
+    '1': {
+      'name': 'one',
+      'speed_number_prefix': 'speed_number_one'
+    },
+    '2': {
+      'name': 'two',
+      'speed_number_prefix': 'speed_number_two'
+    },
+    '3': {
+      'name': 'three',
+      'speed_number_prefix': 'speed_number_three'
+    },
+    '4': {
+      'name': 'four',
+      'speed_number_prefix': 'speed_number_four'
+    },
+    '5': {
+      'name': 'five',
+      'speed_number_prefix': 'speed_number_five'
+    },
+    '6': {
+      'name': 'six',
+      'speed_number_prefix': 'speed_number_six'
+    },
+    '7': {
+      'name': 'seven',
+      'speed_number_prefix': 'speed_number_seven'
+    },
+    '8': {
+      'name': 'eight',
+      'speed_number_prefix': 'speed_number_eight'
+    },
+    '9': {
+      'name': 'nine',
+      'speed_number_prefix': 'speed_number_nine'
+    }
+  };
+
+  let session = sessionService.getSession();
+
+  Object.keys($scope.dial).forEach(function(currentKey) {
+    const databaseRef = firebase.database().ref().child('speed_dials');
+    const querybaseRef = querybase.ref(databaseRef, ['key', 'user_number', 'key_choice']);
+
+    const queriedDbRef = querybaseRef
+      .where({
+        user_number: session.phoneNumber,
+        key: session.uid,
+        key_choice: currentKey
+      });
+
+    console.log(currentKey);
+
+    queriedDbRef.on('value', (snapshot) => {
+      const val = snapshot.val();
+
+      if (val !== null) {
+        const key = Object.keys(val)[0];
+
+        const name = val[key]['name'];
+        const number = val[key]['number'];
+
+        console.log('FOUND! ', currentKey, name, number);
+
+        if (name !== '' && number !== '') {
+          console.log('#' + $scope.dial[currentKey].name, '#' + $scope.dial[currentKey].speed_number_prefix);
+          jQuery('#' + $scope.dial[currentKey].name).val(name);
+          jQuery('#' + $scope.dial[currentKey].speed_number_prefix).val(number);
+
+          jQuery('#' + $scope.dial[currentKey].name).prop('disabled', true);
+          jQuery('#' + $scope.dial[currentKey].speed_number_prefix).prop('disabled', true);
+        }
+      }
+    });
+  });
+
+  $scope.deleteContact = (keychoice) => {
+    console.log(keychoice);
+    let session = sessionService.getSession();
+
+    const databaseRef = firebase.database().ref().child('speed_dials');
+    const querybaseRef = querybase.ref(databaseRef, ['key', 'user_number', 'key_choice']);
+
+    const queriedDbRef = querybaseRef
+      .where({
+        user_number: session.phoneNumber,
+        key: session.uid,
+        key_choice: keychoice
+      });
+
+    queriedDbRef.on('value', (snapshot) => {
+      const val = snapshot.val();
+
+      if (val !== null ) {
+        const key = Object.keys(val)[0];
+
+        console.log('Here2', $scope.dial[keychoice].name);
+        const name = jQuery('#' + $scope.dial[keychoice].name).val();
+        const number = jQuery('#' + $scope.dial[keychoice].speed_number_prefix).val();
+
+        const databaseRef = firebase.database().ref().child(`speed_dials/${key}`);
+        databaseRef.remove().then((resp) => {
+          console.log(resp);
+          jQuery('#' + $scope.dial[keychoice].name).val('');
+          jQuery('#' + $scope.dial[keychoice].speed_number_prefix).val('');
+          jQuery('#' + $scope.dial[keychoice].name).prop('disabled', false);
+          jQuery('#' + $scope.dial[keychoice].speed_number_prefix).prop('disabled', false);
+        });
+      }
+    });
+  }
+
+  $scope.updateSpeedDial = () => {
+    let session = sessionService.getSession();
+    console.log(session);
+
+    for (var i in $scope.dial) {
+      const databaseRef = firebase.database().ref().child('speed_dials');
+      const querybaseRef = querybase.ref(databaseRef, ['key', 'user_number', 'key_choice']);
+
+      const queriedDbRef = querybaseRef
+        .where({
+          user_number: session.phoneNumber,
+          key: session.uid,
+          key_choice: i
+        });
+
+      queriedDbRef.on('value', (snapshot) => {
+        console.log('Here2', $scope.dial[i].name);
+        var name = jQuery('#' + $scope.dial[i].name).val();
+        var number = jQuery('#' + $scope.dial[i].speed_number_prefix).val();
+
+        number = number.replace(/^(\+91)/, "");
+
+        console.log('lol', number, name);
+
+        if (snapshot.val() === null && number !== '' && name !== '') {
+          querybaseRef.push({
+            user_number: session.phoneNumber,
+            key: session.uid,
+            key_choice: i,
+            number: number,
+            name: name
+          });
+          jQuery('#' + $scope.dial[i].name).prop('disabled', true);
+          jQuery('#' + $scope.dial[i].speed_number_prefix).prop('disabled', true);
+        }
+      });
+    }
+  }
 });
 
 
@@ -236,12 +399,34 @@ app.controller("dashboardController", /*@ngInject*/ function($scope, $location, 
 
     let session = sessionService.getSession();
 
-    querybaseRef.push({
-      number: session.phoneNumber,
-      user_id: session.uid,
-      pascode: $scope.strongSecret,
-      name: $scope.name,
-      email: $scope.email
+    const queriedDbRef = querybaseRef
+      .where({
+        number: session.phoneNumber,
+        user_id: session.uid
+      });
+
+    queriedDbRef.on('value', (snapshot) => {
+      if (snapshot.val() === null) {
+        querybaseRef.push({
+          number: session.phoneNumber,
+          user_id: session.uid,
+          passcode: $scope.strongSecret,
+          name: $scope.name,
+          email: $scope.email
+        });
+      } else {
+        const val = snapshot.val();
+        const key = Object.keys(val)[0];
+
+        const databaseRef = firebase.database().ref().child(`totp/${key}`);
+        databaseRef.update({
+          passcode: $scope.strongSecret,
+          name: $scope.name,
+          email: $scope.email
+        }).then((resp) => {
+          console.log(resp);
+        });
+      }
     });
   }
 });
